665ac140537ff334f796e3a068c9f545841e5781
james ramm <james.ramm@offpeaks.com>
2017-01-22 10:24:54 +0000

    Updating settings for whitenoise

--- ramshackleaudio/settings/base.py
+++ ramshackleaudio/settings/base.py
@@ -149,6 +149,8 @@ STATICFILES_DIRS = [
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
