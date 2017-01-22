60149d36b85d96bf42efb510757b4d9d5c69824e
james ramm <james.ramm@offpeaks.com>
2017-01-22 10:02:52 +0000

    revert back tosqlite db

--- ramshackleaudio/settings/base.py
+++ ramshackleaudio/settings/base.py
@@ -114,7 +114,7 @@ REST_FRAMEWORK = {
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
