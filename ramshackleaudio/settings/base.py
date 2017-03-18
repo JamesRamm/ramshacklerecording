46659f495e688d441da4a71186457bcff210853d
james ramm <james.ramm@offpeaks.com>
2017-03-18 20:59:34 +0000

    Use base payment

--- ramshackleaudio/settings/base.py
+++ ramshackleaudio/settings/base.py
@@ -181,7 +181,6 @@ WAGTAIL_SITE_NAME = "ramshackleaudio"
 
 # Longclaw settings
 
-PAYMENT_GATEWAY = 'longclaw.longclawcheckout.gateways.braintree.PaypalVZeroPayment'
 PRODUCT_VARIANT_MODEL = 'products.ProductVariant'
 # Base URL to use when referring to full URLs within the Wagtail admin backend -
 # e.g. in notification emails. Don't include '/admin' or a trailing slash
