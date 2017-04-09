fc118e50361e933b2942d4b0213f3ac4936cdaf0
James Ramm <jamessramm@gmail.com>
2017-04-09 10:43:27 +0100

    Updates

--- products/models.py
+++ products/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 from longclaw.longclawproducts.models import ProductVariantBase, Product
 
+from wagtail.wagtailadmin.edit_handlers import FieldPanel
+
 class ProductVariant(ProductVariantBase):
     stock = models.IntegerField(default=0)
     request_count = models.IntegerField(default=0)
-
