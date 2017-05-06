7b8138fc4cf8ff2d03be35d9883e55314c893110
James Ramm <jamessramm@gmail.com>
2017-05-06 17:08:05 +0100

    Background image

--- home/templatetags/ramshackletags.py
+++ home/templatetags/ramshackletags.py
@@ -14,3 +14,42 @@ def divide(value, arg):
         return int(value) / int(arg)
     except (ValueError, ZeroDivisionError):
         return None
+
+@register.filter
+def rows(thelist, n):
+    """
+    Break a list into ``n`` rows, filling up each row to the maximum equal
+    length possible. For example::
+
+        >>> l = range(10)
+
+        >>> rows(l, 2)
+        [[0, 1, 2, 3, 4], [5, 6, 7, 8, 9]]
+
+        >>> rows(l, 3)
+        [[0, 1, 2, 3], [4, 5, 6, 7], [8, 9]]
+
+        >>> rows(l, 4)
+        [[0, 1, 2], [3, 4, 5], [6, 7, 8], [9]]
+
+        >>> rows(l, 5)
+        [[0, 1], [2, 3], [4, 5], [6, 7], [8, 9]]
+
+        >>> rows(l, 9)
+        [[0, 1], [2, 3], [4, 5], [6, 7], [8, 9], [], [], [], []]
+
+        # This filter will always return `n` rows, even if some are empty:
+        >>> rows(range(2), 3)
+        [[0], [1], []]
+    """
+    try:
+        n = int(n)
+        thelist = list(thelist)
+    except (ValueError, TypeError):
+        return [thelist]
+    list_len = len(thelist)
+    split = list_len // n
+
+    if list_len % n != 0:
+        split += 1
+    return [thelist[split*i:split*(i+1)] for i in range(n)]
