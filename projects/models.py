32f708d400d4172682d3a58120535120e5709f05
James Ramm <jamessramm@gmail.com>
2017-05-09 17:18:18 +0100

    Projects

--- projects/models.py
+++ projects/models.py
@@ -1,25 +1,27 @@
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
+
 from modelcluster.fields import ParentalKey
 from modelcluster.tags import ClusterTaggableManager
 from taggit.models import TaggedItemBase
 
-from wagtail.wagtailcore.models import Page
-from wagtail.wagtailcore.fields import RichTextField, StreamField
+from wagtail.wagtailcore.models import Page, Orderable
+from wagtail.wagtailsnippets.models import register_snippet
+from wagtail.wagtailcore.fields import StreamField
+from wagtail.wagtailcore import blocks
+from wagtail.wagtaildocs.blocks import DocumentChooserBlock
+from wagtail.wagtailimages.blocks import ImageChooserBlock
 from wagtail.wagtailadmin.edit_handlers import FieldPanel, InlinePanel, StreamFieldPanel
 from wagtail.wagtailimages.edit_handlers import ImageChooserPanel
 from wagtail.wagtaildocs.edit_handlers import DocumentChooserPanel
-from wagtail.wagtaildocs.blocks import DocumentChooserBlock
-from wagtail.wagtailcore import blocks
-from wagtail.wagtailimages.blocks import ImageChooserBlock
+from wagtail.wagtailsnippets.edit_handlers import SnippetChooserPanel
+
 
 from longclaw.longclawproducts.models import Product
 from wagtailmath.blocks import MathBlock
 
 class ProjectIndex(Page):
     subpage_types = ["projects.Project"]
-
-class SchematicIndex(Page):
-    subpage_types = ["projects.Schematic"]
 
 class ProjectProduct(models.Model):
     product = models.ForeignKey(Product, related_name="+")
@@ -34,7 +36,7 @@ class ProjectTag(TaggedItemBase):
 
 class Project(Page):
     parent_page_types = ["projects.ProjectIndex"]
-    description = RichTextField(blank=True, null=True)
+    tagline = models.CharField(max_length=128, blank=True, null=True)
     body = StreamField([
         ('heading', blocks.CharBlock(classname="full title")),
         ('paragraph', blocks.RichTextBlock()),
@@ -42,34 +44,34 @@ class Project(Page):
         ('document', DocumentChooserBlock()),
         ('equation', MathBlock())
     ])
+
+    octopart_bom = models.URLField(blank=True, null=True)
     tags = ClusterTaggableManager(through=ProjectTag, blank=True)
 
     @property
     def products(self):
         products = [
             # Grabs rows from the relationship index and formats them correctly.
-            # See the `PostCategoryRelationship` class below.
             n.products for n in self.project_product_relationship.all()
         ]
         return products
 
     # Only inline panels work here.
     content_panels = Page.content_panels + [
-        FieldPanel('description'),
+        FieldPanel('tagline'),
         StreamFieldPanel('body'),
+        FieldPanel('octopart_bom'),
         InlinePanel('project_product_relationship', label='products'),
-        InlinePanel('schematics', label="Schematics"),
+        InlinePanel('schematics', label='schematics'),
         FieldPanel('tags'),
     ]
 
-class SchematicTag(TaggedItemBase):
-    content_object = ParentalKey('Schematic', related_name='tagged_items')
-
-class Schematic(Page):
-    parent_page_types = ["projects.SchematicIndex"]
+@register_snippet
+@python_2_unicode_compatible
+class Schematic(models.Model):
+    name = models.CharField(max_length=32)
     author = models.CharField(max_length=32, blank=True, null=True)
     company = models.CharField(max_length=32, blank=True, null=True)
-    tags = ClusterTaggableManager(through=SchematicTag, blank=True)
     image = models.ForeignKey('wagtailimages.Image',
                               on_delete=models.SET_NULL,
                               related_name='+',
@@ -81,19 +83,27 @@ class Schematic(Page):
                                  on_delete=models.SET_NULL,
                                  related_name='+')
 
-    ramshackle_project = models.ForeignKey('projects.Project',
-                                           null=True,
-                                           blank=True,
-                                           related_name='schematics',
-                                           on_delete=models.SET_NULL)
-
     schematic_url = models.URLField(blank=True, null=True)
 
-    content_panels = Page.content_panels + [
-        DocumentChooserPanel('document'),
+    panels = [
+        FieldPanel('name'),
+        FieldPanel('author'),
+        FieldPanel('company'),
         ImageChooserPanel('image'),
-        FieldPanel('schematic_url'),
-        FieldPanel("author"),
-        FieldPanel("company"),
-        FieldPanel('tags'),
+        DocumentChooserPanel('document'),
+        FieldPanel('schematic_url')
     ]
+
+    def __str__(self):
+        return self.name
+
+
+class ProjectSchematic(Orderable, models.Model):
+    project = ParentalKey(Project, related_name="schematics")
+    schematic = models.ForeignKey(Schematic, related_name='+')
+    panels = [
+        SnippetChooserPanel('schematic'),
+    ]
+
+    def __str__(self):
+        return self.project.title + " -> " + self.schematic.name
